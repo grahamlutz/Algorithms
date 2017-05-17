c305b23b0091da8be0167d934f3fa746ce7eba81
Graham Lutz <graham@brainbytescreative.com>
2017-05-17 17:52:48 -0400

    adds insertion sort in javascript

---
+++ insertionSort.js
@@ -0,0 +1,21 @@
+//
+// Insertion Sort
+// 
+
+var insert = function(array, rightIndex, value) {
+    for(var i = rightIndex; i >= 0 && array[i] > value; i--) {
+        array[i + 1] = array[i];
+    }   
+    array[i + 1] = value; 
+};
+
+var insertionSort = function(array) {
+    for( var i = 1; i < array.length; i++){
+        insert(array,i-1,array[i]);
+    }
+    return array
+};
+
+var array = [22, 11, 99, 88, 9, 7, 42];
+var sortedArray = insertionSort(array);
+console.log(sortedArray)
