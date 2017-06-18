1de1c95d2eb6688ff4d89ca81417d52e5bd8ff29
Graham Lutz <graham@brainbytescreative.com>
2017-06-18 19:39:06 -0400

    builds basic HashTable class

--- python/hash.py
+++ python/hash.py
@@ -1,9 +1,29 @@
-def hash(astring, table_size):
-	'''
-	Returns remainder of dividing the sum of the strings ordinals by
-	the size of the table.
-	'''
-	sum = 0
-	for s in range(len(astring)):
-		sum = sum + ord(asing[s])
-	return sum % table_size
+class HashTable(size):
+	def __init__(self, size):
+		self.size = size || 11
+		self.slots = [None] * self.size
+		self.data  = [None] * self.size
+
+	def get():
+		return
+
+	def put():
+		return
+
+	def __getitem__(self, key):
+
+	def __setitem__(self, ket, data):
+
+
+	def hash_function(astring, table_size):
+		'''
+		Returns remainder of dividing the sum of the strings ordinals by
+		the size of the table.
+		'''
+		sum = 0
+		for s in range(len(astring)):
+			sum = sum + ord(asing[s])
+		return sum % table_size
+
+	def rehash():
+		return
