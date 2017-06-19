d3c2dd20616a447134dc626c06f198ac3a3d8e2f
Graham Lutz <graham@brainbytescreative.com>
2017-06-19 06:46:18 -0400

    implements put function

--- python/hash.py
+++ python/hash.py
@@ -7,23 +7,46 @@ class HashTable(size):
 	def get():
 		return
 
-	def put():
-		return
+	def put(self, key, data):
+		hash_value = self.hash_function(key, len(self.slots))
+		is_slot_empty = self.slots[hash_value] == None
+		is_key_in_slot = self.slots[hash_value] == key
+
+		if is_slot_empty:
+			self.slots[hash_value] = key
+			self.data[hash_value] = data
+		else:
+			if is_key_in_slot:
+				self.data[hash_value] = data
+			else:
+				next_slot = self.rehash(hash_value, len(self.slots))
+				while self.slots[next_slot] != None and self.slots[next_slot] != key:
+					next_slot = self.rehash(next_slot, len(self.slots))
+
+				if self.slots[next_slot] == None:
+					self.slots[next_slot] = key
+					self.data[next_slot] = data
+				else:
+					self.data[next_slot] = data
+
+
 
 	def __getitem__(self, key):
+		return self.get(key)
 
-	def __setitem__(self, ket, data):
+	def __setitem__(self, key, data):
+		return self.put(key, data)
 
-
-	def hash_function(astring, table_size):
+	def hash_function(self, key, size):
 		'''
 		Returns remainder of dividing the sum of the strings ordinals by
 		the size of the table.
 		'''
-		sum = 0
-		for s in range(len(astring)):
-			sum = sum + ord(asing[s])
-		return sum % table_size
+		# sum = 0
+		# for s in range(len(astring)):
+		# 	sum = sum + ord(asing[s])
+		# return sum % table_size
+		return key % size
 
 	def rehash():
 		return
