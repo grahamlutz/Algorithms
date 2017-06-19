abb9c858bf8d219ceba7035fd1149bc75d95a37f
Graham Lutz <graham@brainbytescreative.com>
2017-06-19 12:26:27 -0400

    first run at testing how many picks it takes to get all M&M colors

---
+++ python/number_of_mms.py
@@ -0,0 +1,47 @@
+'''
+How many times must you pick to guarantee you've picked all colors of M&M's?
+
+Constraints:
+
+1) There are n colors of M&M's
+2) There is an equal number of each M&M
+3) We don't know the total number of M&M's
+'''
+
+import matplotlib.pylab as plt
+import numpy as np
+
+def run(n):
+    """
+    n: number of different M&M's
+    return: number of picks it took to get all 10
+    """
+    picks = 0
+    picked = set()
+    while True:
+        # check if we have enough
+        if len(picked) == n:
+            break
+        # pick an M&M
+        got = np.random.randint(0, n)  # choose a M&M, randomly 1/n
+        picks += 1
+        picked.add(got)
+
+    return picks
+
+def run_sequence(n, trials):
+    for y in range(1, n+1):
+        totalpicks = 0
+
+        for x in range(1, trials):
+            totalpicks += run(y)
+
+        print(totalpicks / float(trials))
+        plt.ion()
+        plt.figure("M&M's Picked")
+        plt.plot(y,(totalpicks / float(trials)), marker='o', ms = 10, alpha=1, color='b')
+
+    plt.show("M&M's Picked")
+
+if __name__ == '__main__':
+    run_sequence(10, 1000)
