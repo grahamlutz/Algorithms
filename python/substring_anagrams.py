89dda84b04b0e9351259329ef604f3ac37fb21a4
Graham Lutz <graham@brainbytescreative.com>
2017-06-25 15:21:30 -0400

    adds unsolved substring_anagrams.py

---
+++ python/substring_anagrams.py
@@ -0,0 +1,15 @@
+'''
+ Given a smaller string 5 and a bigger string b, design an algorithm to find all permutations
+of the shorter string within the longer one. Print the location of each permutation. 
+'''
+
+def substring_anagram(short, long):
+	short_len = len(short)
+	long_len  = len(long)
+	locations = []
+
+	print locations
+
+
+substring_anagram('abbc', 'cbabadcbbabbcbabaabccbabc')
+
