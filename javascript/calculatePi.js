3b0de833b243da944db17250953efde158c53344
Graham Lutz <graham@brainbytescreative.com>
2017-06-26 17:32:30 -0400

    updates calculatePi to use ES7 ** for powers

---
+++ javascript/calculatePi.js
@@ -0,0 +1,23 @@
+
+function pi(pointsArray) {
+	let pointsInsideDisk = 0;
+
+	pointsArray.map( (point) => {
+		if ( (point[0] ** 2) + (point[1] ** 2) <= 1 ) {
+			pointsInsideDisk++;
+		}
+	})
+
+	let pi = (pointsInsideDisk / pointsArray.length) * 4
+
+	return pi
+}
+
+let points = [];
+for (let i = 0; i < 10000; i++) {
+	let x = Math.random()
+	let y = Math.random()
+	points.push([x,y])
+}
+
+console.log(pi(points));
