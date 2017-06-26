6ded81819b1ebdec79c857beea4e9ad9915ffb42
Graham Lutz <graham@brainbytescreative.com>
2017-06-26 18:30:58 -0400

    shorten if statement

--- javascript/calculatePi.js
+++ javascript/calculatePi.js
@@ -3,9 +3,7 @@ function pi(pointsArray) {
 	let pointsInsideDisk = 0;
 
 	pointsArray.map( (point) => {
-		if ( (point[0] ** 2) + (point[1] ** 2) <= 1 ) {
-			pointsInsideDisk++;
-		}
+		if ( (point[0] ** 2) + (point[1] ** 2) <= 1 ) pointsInsideDisk++
 	})
 
 	let pi = (pointsInsideDisk / pointsArray.length) * 4
